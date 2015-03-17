c4d1879f0b3b42860de4985a0a87f22535d79549
nekosaur <albert@kaaman.nu>
2015-03-17 21:09:39 +0100

    Cleaned up code, using my fork of python-unrar to get access to extractall with callback parameter

--- unpacker.py
+++ unpacker.py
@@ -2,10 +2,12 @@ import regex
 import click
 import os
 import ctypes
+from progressbar import ProgressBar, Percentage, Bar
 from path import Path
 from unrar import rarfile
 from unrar import unrarlib
 from unrar import constants
+
 
 #pattern = regex.compile('(?V1)(?<!part[\d]+)\.rar')
 first_part = regex.compile(r"(?V1)(.*)(\.part1|(?<!\.part\d+))\.rar$", regex.I)
@@ -14,8 +16,9 @@ first_part = regex.compile(r"(?V1)(.*)(\.part1|(?<!\.part\d+))\.rar$", regex.I)
 
 @click.command()
 @click.argument('path', type=click.Path(exists=True,file_okay=False,resolve_path=True))
-@click.option('--recursive', is_flag=True, help='Makes unpacker look in subdirectories')
+@click.option('--recursive', is_flag=True, help='Unpacker will search subdirectories')
 @click.option('--clean', is_flag=True, help='Unpacker will delete archives after successful extraction')
+#@click.option('--free', type=click.)
 def cli(path, recursive, clean):
     '''Unpacker will unpack RAR files in a chosen directory and 
         optionally delete them after successful extraction.
@@ -23,26 +26,17 @@ def cli(path, recursive, clean):
         Unpacker will only process the first valid RAR file 
         found in a directory. It can handle multi-part archives
         in both forms (.partX.rar and .rXX)'''
-    click.echo(path)
-    click.echo(recursive)
-    click.echo(clean)
+
+    click.echo("Recursive = %s" % recursive)
+    click.echo("Cleaning = %s" % clean)
 
     archives = find_archives(path, recursive)
 
-    #for archive in archives:
-    #   unpack_archive(archive)
-    #   if (clean):
-    #       delete_archive(archive)
-
-    #with click.progressbar(length=1000000) as test:
-    #    while not test.finished:
-    #        test.next(100)
-
-    #return
-
     for archive in archives:
-        click.echo(archive.abspath)
+        click.echo("Extracting file %s" % archive.abspath)
         archive.unpack(clean)
+
+    click.echo("All Done!")
 
 def do(x):
     return False
@@ -65,9 +59,9 @@ def find_archives(path, recursive):
 
 def is_first_archive(filepath):
     if rarfile.is_rarfile(filepath):
-        click.echo("File %s is RAR archive" % filepath)
+        #click.echo("File %s is RAR archive" % filepath)
         if first_part.search(filepath):
-            click.echo("And should be first archive")
+            #click.echo("And should be first archive")
             return True
     return False
 
@@ -90,96 +84,49 @@ class Archive:
             if rarfile.is_rarfile(filepath):
                 if multi_part.search(filepath):
                     parts.append(item)
-                    click.echo("File %s added to parts" % item.abspath())
 
         return parts
 
-    def callback(self, msg, user_data, p1, p2):
-        if msg == constants.UCM_PROCESSDATA:
-            click.echo(p2)
-        return 1
-
-    def _open(self, archive):
-        """Open RAR archive file."""
-        try:
-            handle = unrarlib.RAROpenArchiveEx(ctypes.byref(archive))
-        except unrarlib.UnrarException:
-            raise BadRarFile("Invalid RAR file.")
-        return handle
-        
-    def _close(self, handle):
-        """Close RAR archive file."""
-        try:
-            unrarlib.RARCloseArchive(handle)
-        except unrarlib.UnrarException:
-            raise BadRarFile("RAR archive close error.")
-
-    def _read_header(self, handle):
-        """Read current member header into a RarInfo object."""
-        rarinfo = None
-        header_data = unrarlib.RARHeaderDataEx()
-        res = unrarlib.RARReadHeaderEx(handle, ctypes.byref(header_data))
-        if res != constants.ERAR_END_ARCHIVE:
-            rarinfo = rarfile.RarInfo(header=header_data)
-        return rarinfo
-
-    def _process_current(self, handle, op, dest_path=None, dest_name=None):
-        """Process current member with 'op' operation."""
-        unrarlib.RARProcessFileW(handle, op, dest_path, dest_name)
-
     def get_total_size(self):
         total_size = 0
-        archive = unrarlib.RAROpenArchiveDataEx(
-            self.abspath, mode=constants.RAR_OM_EXTRACT)
-        handle = self._open(archive)
+        rar = rarfile.RarFile(self.abspath)
 
-        click.echo("Getting total size")
-
-        try:
-            rarinfo = self._read_header(handle)
-            while rarinfo is not None:
-                total_size += rarinfo.file_size
-                rarinfo = self._read_header(handle)
-        except unrarlib.UnrarException:
-            raise BadRarFile("Bad RAR archive data.")
-        finally:
-            self._close(handle)
+        previous_entry = u""
+        for rarinfo in rar.infolist():
+            # filelist was generated with mode RAR_OM_LIST_INCSPLIT
+            # which means spanning files get two entries.
+            if previous_entry == rarinfo.filename:
+                continue
+            #click.echo("Found archived file %s " % rarinfo.filename)
+            total_size += rarinfo.file_size
+            previous_entry = rarinfo.filename
 
         click.echo("Total size = %dkB" % (int(total_size) / 1000))
+
         return total_size
 
     def unpack(self, clean=False, destination=None):
-        # TODO: Check for free space
         if (destination == None):
             destination = self.dirpath
 
         callback = ProgressCallback(self.get_total_size())
-        c_callback = unrarlib.UNRARCALLBACK(callback._callback)
 
-        archive = unrarlib.RAROpenArchiveDataEx(
-            self.abspath, mode=constants.RAR_OM_EXTRACT)
-        handle = self._open(archive)
-        
-        unrarlib.RARSetCallback(handle, c_callback, 0)
-        
         try:
-            rarinfo = self._read_header(handle)
-            with click.progressbar(length=callback._total_size) as callback._bar:
-                while rarinfo is not None:
-                    self._process_current(
-                        handle, constants.RAR_EXTRACT, destination)
-                    rarinfo = self._read_header(handle)
-        except unrarlib.UnrarException:
-            raise BadRarFile("Bad RAR archive data.")
-        finally:
-            self._close(handle)
+            rar = rarfile.RarFile(self.abspath)
+            rar.extractall(destination, callback=callback._callback)
+        except rarfile.BadRarFile:
+            click.echo("Failed to extract %s" % self.abspath)
+            return
+
+        click.echo("Done!")
 
         if clean:
-            click.echo("Removing RAR file(s)")
+            click.echo("Removing RAR file(s):")
             for file in self.parts:
                 click.echo(file)
                 os.remove(file)
             # Make sure first part is deleted
+            click.echo(self.abspath)
             os.remove(self.abspath)
 
     def delete(self):
@@ -188,9 +135,14 @@ class Archive:
 class ProgressCallback(object):
     def __init__(self, total_size):
         self._total_size = total_size
-        self._bar = None
+        self._read_size = 0
+        self._bar = ProgressBar(widgets=[Percentage(), Bar()], maxval=total_size).start()
 
     def _callback(self, msg, user_data, p1, p2):
         if msg == constants.UCM_PROCESSDATA:
-            self._bar.next(p2)
+            self._read_size += p2
+            self._bar.update(self._read_size)
+            if self._read_size >= self._total_size:
+                self._bar.finish()
         return 1
+
